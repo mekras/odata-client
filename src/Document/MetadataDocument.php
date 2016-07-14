0cbad3ba289b4b8946a38b2fc9ddf674c2e2c45e
Михаил Красильников <m.krasilnikov@yandex.ru>
2016-07-14 17:54:53 +0300

    MetadataDocument stub.

--- src/Document/MetadataDocument.php
+++ src/Document/MetadataDocument.php
@@ -12,8 +12,31 @@ namespace Mekras\OData\Client\Document;
  *
  * @since 1.0
  *
- * @link http://www.odata.org/documentation/odata-version-2-0/overview/#ServiceMetadataDocument
+ * @link  http://www.odata.org/documentation/odata-version-2-0/overview/#ServiceMetadataDocument
  */
 class MetadataDocument extends Document
 {
+    /**
+     * Return node main namespace.
+     *
+     * @return string
+     *
+     * @since 1.0
+     */
+    public function ns()
+    {
+        return 'http://schemas.microsoft.com/ado/2007/06/edmx';
+    }
+
+    /**
+     * Return root node name here.
+     *
+     * @return string
+     *
+     * @since 1.0
+     */
+    protected function getRootNodeName()
+    {
+        return 'Edmx';
+    }
 }
